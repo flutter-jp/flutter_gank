53f28c9fdc575a36c2a20e7d15cee0ef1500b95f
xiaomo <83387856@qq.com>
2017-04-25 20:09:47 +0800

    set player

--- ykLive/ViewController.swift
+++ ykLive/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBg()
+        setPlayerView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
